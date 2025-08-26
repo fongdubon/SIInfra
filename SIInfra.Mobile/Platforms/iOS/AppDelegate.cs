683545477aa18177b62b1610b6aa4e9e7f4cd489
eduardofongdubon@hotmail.com <eduardofongdubon@hotmail.com>
2025-08-26 08:38:17 -0600

    Arquitectura de proyecto Proyecto web Proyecto Mobile Proyecto Shared

---
+++ SIInfra.Mobile/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SIInfra.Mobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
---
+++ SIInfra.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SIInfra.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
